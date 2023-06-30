678f5bc4a713e4e96fa14b3bc76d6c2e5d3053ca
konotopdv <konotopdv@gmail.com>
2023-06-30 08:34:23 +0200

    task_3006

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,23 +1,42 @@
-const url = 'https://api.thecatapi.com/v1';
-const api_key =
-  'live_DVnU0ZYbgHx4Gq16lymrax09mhyq6Jcimr8xzqh0EDlDYpfW7wM6568eXBaVraX3';
+// const url = 'https://api.thecatapi.com/v1';
+// const api_key =
+//   'live_DVnU0ZYbgHx4Gq16lymrax09mhyq6Jcimr8xzqh0EDlDYpfW7wM6568eXBaVraX3';
 
+// export function fetchBreeds() {
+//   return fetch(`${url}/breeds?api_key=${api_key}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+
+// export function fetchCatByBreed(breedId) {
+//   return fetch(
+//     `${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+const API_KEY =
+  'live_DVnU0ZYbgHx4Gq16lymrax09mhyq6Jcimr8xzqh0EDlDYpfW7wM6568eXBaVraX3';
 export function fetchBreeds() {
-  return fetch(`${url}/breeds?api_key=${api_key}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return fetch(`https://api.thecatapi.com/v1/breeds`).then(response =>
+    response.json()
+  );
 }
 
 export function fetchCatByBreed(breedId) {
   return fetch(
-    `${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+    `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${breedId}&has_breeds=1`
+  ).then(response => response.json());
+}
+
+export function fetchInfoById(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/${breedId}?api_key=${API_KEY}`
+  ).then(response => response.json());
 }
--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const ref = {
 };
 const { selector, divCatInfo, loader, error } = ref;
 
-loader.classList.replace('loader', 'is-hidden');
 error.classList.add('is-hidden');
 divCatInfo.classList.add('is-hidden');
+selector.classList.add('is-hidden');
 
 let arrBreedsId = [];
 fetchBreeds()
@@ -22,31 +22,41 @@ fetchBreeds()
     data.forEach(element => {
       arrBreedsId.push({ text: element.name, value: element.id });
     });
+    const emptyObj = { text: '', value: '' };
+    arrBreedsId.unshift(emptyObj);
     new SlimSelect({
       select: selector,
       data: arrBreedsId,
     });
+    selector.classList.remove('is-hidden');
   })
-  .catch(onFetchError);
+  .catch(onFetchError)
+  .finally(() => {
+    loader.classList.add('is-hidden');
+  });
 
 selector.addEventListener('change', onSelectBreed);
 
 function onSelectBreed(event) {
-  loader.classList.replace('is-hidden', 'loader');
-  selector.classList.add('is-hidden');
-  divCatInfo.classList.add('is-hidden');
+  loader.classList.remove('is-hidden');
 
   const breedId = event.currentTarget.value;
-  fetchCatByBreed(breedId)
-    .then(data => {
-      loader.classList.replace('loader', 'is-hidden');
-      selector.classList.remove('is-hidden');
-      const { url, breeds } = data[0];
+  if (breedId) {
+    fetchCatByBreed(breedId)
+      .then(data => {
+        loader.classList.replace('loader', 'is-hidden');
+        selector.classList.replace('selector', 'is-hidden');
+        const { url, breeds } = data[0];
 
-      divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
-      divCatInfo.classList.remove('is-hidden');
-    })
-    .catch(onFetchError);
+        divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+        divCatInfo.classList.remove('is-hidden');
+      })
+
+      .catch(onFetchError)
+      .finally(() => {
+        loader.classList.add('is-hidden');
+      });
+  }
 }
 
 function onFetchError(error) {
@@ -63,3 +73,95 @@ function onFetchError(error) {
     }
   );
 }
+// import { fetchBreeds, fetchCatByBreed, fetchInfoById } from './cat-api';
+// import SlimSelect from 'slim-select';
+// import Notiflix from 'notiflix';
+// import './styles.css';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const API_KEY =
+//   'live_DVnU0ZYbgHx4Gq16lymrax09mhyq6Jcimr8xzqh0EDlDYpfW7wM6568eXBaVraX3';
+
+// //елементи ДОМ
+// const refs = {
+//   select: document.querySelector('.breed-select'),
+//   catInfo: document.querySelector('.cat-info'),
+//   loader: document.querySelector('.loader'),
+// };
+// const textErroeEl = refs.loader.nextElementSibling;
+
+// fetchBreeds(); // виклик функції, що додає в select елементи option
+
+// // if (fetchBreeds().promiseState === "pending") {
+// //       refs.loader.classList.remove("is-hidden");
+// // }
+
+// // запит на масив об'єктів всіх порід
+// // передирання та рендер розмітки до select
+// fetchBreeds()
+//   .then(response => {
+//     const arrayOptions = response
+//       .map(element => `<option value=${element.id}> ${element.name} </option>`)
+//       .join(); //перший метод рендеру
+
+//     refs.select.innerHTML = arrayOptions;
+
+//     const customSelect = new SlimSelect({ select: '#single' });
+
+//     refs.loader.classList.add('is-hidden');
+//     refs.select.classList.remove('is-hidden');
+//     textErroeEl.classList.add('is-hidden');
+//     // response.forEach(element => { //другий метод рендеру
+//     // //    console.log(element)
+//     //    refs.select.insertAdjacentHTML("beforeend",
+//     //         `<option value=${element.id}> ${element.name} </option>`)
+//     // });
+//   })
+//   .catch(err => console.warn(err));
+
+// //Відстежування події зміни select
+// // в константу breedId записуємо значення
+
+// refs.select.addEventListener('change', function () {
+//   textErroeEl.classList.add('is-hidden'); //приховуємо сповіщеня, якщо раніше була помилка
+
+//   const breedId = this.value;
+//   refs.catInfo.innerHTML = ''; // якщо є вже розмітка видаляємо
+
+//   refs.loader.classList.remove('is-hidden'); // видаляємо селектор, щоб відзначити завантаження
+
+//   fetchCatByBreed(breedId)
+//     .then(res => {
+//       res.forEach(element => {
+//         refs.catInfo.insertAdjacentHTML(
+//           'beforeend',
+//           `<img class="photo-cat" src=${element.url}>`
+//         ); //рендер фото
+
+//         fetchInfoById(element.id)
+//           .then(resp => {
+//             // по ід знаходимо інформацію про кота + додаємо розмітку
+
+//             console.log(resp); //повертається масив з одним об'єктом
+
+//             refs.catInfo.insertAdjacentHTML(
+//               'beforeend',
+//               `<h1>${resp.breeds[0].name}</h1>
+//                 <p>${resp.breeds[0].description}</p>
+//                 <p><b>Temperament: </b>${resp.breeds[0].temperament}</p>`
+//             );
+//           })
+//           .catch(err => console.log(err));
+//       });
+//     })
+//     .catch(err => {
+//       console.warn(err);
+//       textErroeEl.classList.remove('is-hidden');
+//       Notiflix.Notify.failure(
+//         'Oops! Something went wrong! Try reloading the page!'
+//       );
+//     })
+//     .finally(() => {
+//       refs.loader.classList.add('is-hidden');
+//     }); //в будь якому разі видаляємо позначення завантаження
+// });
